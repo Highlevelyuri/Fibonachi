9b6a3e818a3608366eb565c320bf017d010cf34a
Highlevelyuri <kriucov61944zce3.com@gmail.com>
2018-02-14 16:23:00 +0200

    Initial commit

---
+++ src/com/company/fbnch.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class fbnch {
+     void  rew ( int n){
+         int sum;
+        for ( n=1; n<500; ){
+            sum = (n-1)+ (n-2);
+
+        }
+    }
+
+    public void rew() {
+    }
+}
